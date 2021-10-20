a40bd50b5c639dab33fc27967f0c8648d9393013
Zülal Nur Hıdıroğlu <nur.hidiroglu@ug.bilkent.edu.tr>
2021-10-20 18:47:26 +0300

    Update MaterialManager.py

--- src/MaterialManager.py
+++ src/MaterialManager.py
@@ -24,5 +24,12 @@ class MaterialManager:
 
         return self._announceMaterial(returnText)
 
-    def _announceMaterial(self, text: str) -> bool:
-        return self.announcer(text)
+def _announceMaterial(self, text: str) -> bool:
+   returnValue = False
+   try:
+       returnValue = self.announcer(text)
+   except:
+       print("Announcement of material type is failed.")
+       returnValue = False
+   finally:
+       return returnValue
